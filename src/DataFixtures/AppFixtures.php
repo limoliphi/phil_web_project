19f19de89a3382461fa6a19fe4ae88952c47ac88
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-11-09 19:44:39 +0100

    Add fixtures to user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        $user = new User();
+
+        $user
+            ->setEmail('user@test.com')
+            ->setPrenom($faker->firstName())
+            ->setNom($faker->lastName())
+            ->setTelephone($faker->phoneNumber())
+            ->setAPropos($faker->text())
+            ->setInstagram('instagram');
+
+        $password = $this->encoder->encodePassword($user, 'password');
+        $user->setPassword($password);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
