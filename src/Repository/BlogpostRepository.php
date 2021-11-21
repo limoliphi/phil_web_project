5dc441af48f348a116df66fe580dbbe45376407c
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-11-21 10:37:37 +0100

    Delete commented code

--- src/Repository/BlogpostRepository.php
+++ src/Repository/BlogpostRepository.php
@@ -18,33 +18,4 @@ class BlogpostRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Blogpost::class);
     }
-
-    // /**
-    //  * @return Blogpost[] Returns an array of Blogpost objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Blogpost
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/OeuvreRepository.php
+++ src/Repository/OeuvreRepository.php
@@ -18,33 +18,4 @@ class OeuvreRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Oeuvre::class);
     }
-
-    // /**
-    //  * @return Oeuvre[] Returns an array of Oeuvre objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('o.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Oeuvre
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
