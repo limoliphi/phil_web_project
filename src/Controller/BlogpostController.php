94605f2522447c2b7fc26b11816fcb408b967c63
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-11-28 17:56:08 +0100

    Add pagination for actualites and some fixes to nav and footer

---
+++ src/Controller/BlogpostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BlogpostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogpostController extends AbstractController
+{
+    /**
+     * @Route("/actualites", name="actualites")
+     */
+    public function index(
+        BlogpostRepository $blogpostRepository,
+        PaginatorInterface $paginator,
+        Request $request
+
+    ): Response {
+        $data = $blogpostRepository->findAll();
+
+        $blogposts = $paginator->paginate($data, $request->query->getInt('page', 1), 6);
+
+        return $this->render('blogpost/index.html.twig', [
+            'blogposts' => $blogposts,
+        ]);
+    }
+}
