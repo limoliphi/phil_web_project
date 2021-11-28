d1d4f0ed1985146aaf1296cf62fc4178dea82dde
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-11-28 17:17:09 +0100

    Add pagination to RealisationsController

--- src/Controller/RealisationsController.php
+++ src/Controller/RealisationsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\OeuvreRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,17 @@ class RealisationsController extends AbstractController
     /**
      * @Route("/realisations", name="realisations")
      */
-    public function index(OeuvreRepository $oeuvreRepository): Response
-    {
+    public function index(
+        OeuvreRepository $oeuvreRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $data = $oeuvreRepository->findAll();
+
+        $oeuvres = $paginator->paginate($data, $request->query->getInt('page', 1), 6);
+
         return $this->render('realisations/index.html.twig', [
-            'oeuvres' => $oeuvreRepository->findAll(),
+            'oeuvres' => $oeuvres,
         ]);
     }
 }
